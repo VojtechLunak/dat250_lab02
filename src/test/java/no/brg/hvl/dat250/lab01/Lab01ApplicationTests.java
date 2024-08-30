6685f686d35836312fb1c7e6a5fc91a9526598c3
Vojtěch Luňák <vojtech.lunak1@gmail.com>
2024-08-30 20:59:03 +0200

    init()

---
+++ src/test/java/no/brg/hvl/dat250/lab01/Lab01ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.brg.hvl.dat250.lab01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
