6a59a02992906ddc1508711b91900965abc27d8d
Vojtěch Luňák <vojtech.lunak1@gmail.com>
2024-08-30 21:05:25 +0200

    fix(renames and build)

---
+++ src/test/java/no/brg/hvl/dat250/lab02/Lab02ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.brg.hvl.dat250.lab02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
