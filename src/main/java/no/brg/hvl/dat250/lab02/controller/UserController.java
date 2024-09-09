8c6241778d32ff3d2e0fe6927aa8ab8b41b7a179
Vojtěch Luňák <vojtech.lunak1@gmail.com>
2024-09-09 10:13:01 +0200

    fix(cleanup)

--- src/main/java/no/brg/hvl/dat250/lab02/controller/UserController.java
+++ src/main/java/no/brg/hvl/dat250/lab02/controller/UserController.java
@@ -1,9 +1,7 @@
 package no.brg.hvl.dat250.lab02.controller;
 
-import no.brg.hvl.dat250.lab02.controller.wrapper.AddVoteRequestWrapper;
 import no.brg.hvl.dat250.lab02.model.User;
 import no.brg.hvl.dat250.lab02.model.Vote;
-import no.brg.hvl.dat250.lab02.model.VoteOption;
 import no.brg.hvl.dat250.lab02.service.PollManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
